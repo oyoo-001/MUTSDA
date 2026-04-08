f6ded726818616c934a21a89329126c35c685cfc
byron-003 <byronoyoo2030@gmail.com>
2026-04-08 17:51:47 +0300

    google auth nav issue

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 // Unregister any existing service workers to fix "Failed to fetch" errors from stale/broken workers
 if ('serviceWorker' in navigator) {
@@ -17,5 +18,7 @@ if ('serviceWorker' in navigator) {
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+    <App />
+  </GoogleOAuthProvider>
 )
